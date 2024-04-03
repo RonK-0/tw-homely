d02f39299ba2c33768188c5e5fe8a38405132a8c
RonK-0 <ronkua12@gmail.com>
2024-04-03 10:31:10 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,38 @@
+// AOS init and set animations to occur only once
+// AOS.init({ once: true });
+
+const burg = document.querySelector(".burger");
+const nav = document.querySelector(".nav__links");
+
+// nav burger toggle mobile
+burg.addEventListener("click", ()=>{
+    burg.classList.toggle("show");
+    nav.classList.toggle("show");
+});
+
+// const rt = document.querySelector("body");
+// const togglebtn = document.querySelector(".toggleBtn");
+// togglebtn.addEventListener("click", ()=>{
+//     rt.classList.toggle("dark");
+// });
+
+// auto close nav menu on click of any link for mobile
+// const menu_entry = document.querySelectorAll("#menu-item");
+// menu_entry.forEach((item) => {
+//     item.addEventListener("click", ()=>{
+//         burg.classList.remove("active");
+//         nav.classList.remove("open");
+//     });
+// });
+
+// on scroll show back to top button
+// window.onscroll = function() { 
+//     var btt = document.querySelector('.backTop');
+//     var pageBanner = document.querySelector('.pageBanner');
+//     var bannerHeight = pageBanner.offsetHeight*0.025
+//     if ( window.scrollY > bannerHeight ) { btt.classList.add("shown"); }
+//     else { btt.classList.remove("shown"); }
+// }
+
+// scroll to top function
+// function scrollToTop(){ window.scrollTo(0,0); }
---
+++ tailwind.config.js
@@ -0,0 +1,83 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      screens: {
+        // 'sm': '640px',
+        // 'lg': '1024px',
+        // 'xl': '1280px',
+        // '2xl': '1536px',
+        'xs': '490px',
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        '2xl': '1400px',
+      },
+      backgroundImage:{
+        transparent_bg: "url('../dist/img/transparent.png')",
+        home_banner_bg: "url('../dist/img/home/banner-bg.png')",
+        footer_bg: "url('../dist/img/footer-bg.png')",
+      },
+      colors:{
+        dark: "#0D0D0D",
+        dark_2: "#1B1C1E",
+        light: "#FAFAFA",
+        light_2: "#D6D8DB",
+        d_gray: "#2C2D2F",
+        gray: "#797D86",
+        primary: "#A57A5A",
+      },
+      fontFamily: {
+        'text-poppins': ["Poppins", "sans-serif"],
+        'text-inter': ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        'text_xs' : 'clamp(11px, 3vw, 14px)',
+        'text_sm' : 'clamp(13px, 2.5vw, 14px)',
+        'text_md' : 'clamp(13.5px, 1.75vw, 16px)',
+        'text_lg' : 'clamp(14px, 1.5vw, 18px)',
+
+        'fs-33': 'clamp(28px, 1.66vw, 42px)',
+        'fs-19_md' : 'clamp(16px, 1vw, 26px)',
+        'fs-19_sm': 'clamp(16px, 1.563vw, 18px)',
+        'fs-19': 'clamp(14px, 1.778vw, 16px)',
+        'fs-16_xl': 'clamp(14px, 0.833vw, 22px)',
+        'fs-16_lg': 'clamp(14px, 1.563vw, 18px)',
+        'fs-16_md': 'clamp(13.5px, 1.778vw, 16px)',
+        'fs-16': 'clamp(11px, 3.59vw, 14px)',
+
+        'banner_h1' : 'clamp(64px, 5.156vw, 150px)',
+
+        'button' : 'clamp(16px, 1vw, 24px)'
+      },
+      listStyleType: {
+      //   none: 'none',
+      // circle: 'circle',
+      //   disc: 'disc',
+      //   decimal: 'decimal',
+      //   square: 'square',
+      //   roman: 'upper-roman',
+      },
+      backgroundSize: {
+        // '400%': '400% 400%'
+      },
+      animation: {
+        // 'bannerSlide': 'bannerSlide 15s ease forwards',
+      },
+      keyframes: {
+        // 'bannerSlide': {
+        //   '0%' : {'background-position': '25% 0%'},
+        //   '100%' : {'background-position': '40% 80%'}
+        // },
+      },
+      boxShadow:{
+        'top_button_shadow'     : '4px 4px 10px rgba(0, 0, 0, 0.2)',
+
+      },
+    },
+  },
+  plugins: [],
+}
